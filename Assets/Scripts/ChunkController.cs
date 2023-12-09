4a6c14f8a67a499f7e2f7dcb677ce7ef1eb087ed
Alex Bard <darlbard@gmail.com>
2023-12-09 22:52:48 +0200

    Week4Complete

---
+++ Assets/Scripts/ChunkController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkController : MonoBehaviour
+{
+    public float Speed;
+    public float ChunkSize;
+    public List<Chunk> ChunksInScene;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < ChunksInScene.Count; i = i + 1)
+        {
+            ChunksInScene[i].transform.Translate(Vector3.back * Speed * Time.deltaTime);
+            if (ChunksInScene[i].transform.position.z < -ChunkSize)
+            {
+                ChunksInScene[i].transform.Translate(Vector3.forward * ChunkSize * ChunksInScene.Count);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Offset;
+    private float Position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && Position > -1) {
+            Position = Position - 1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && Position < 1)
+        {
+            Position = Position + 1;
+        }
+        //transform.position = new Vector3(Position*Offset, 0, 0);
+        transform.position = Vector3.MoveTowards(transform.position,  new Vector3(Position * Offset, 0, 0), Time.deltaTime*10);
+    }
+}
